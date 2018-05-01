25207719bb75937ed24ccd4d18680e2f8514a2e5
Alex Kolchinski <y.a.kolchinski@gmail.com>
2018-05-01 11:59:55 -0700

    Removed unused code

--- src/baselines.py
+++ src/baselines.py
@@ -48,32 +48,6 @@ def get_embeddings_and_sum_phi(reader, get_embed_fn):
     return get_embed_sum_phi(embeds)
 
 
-# Define a comment as unigram features
-#def unigrams_phi_c(comment):
-#    return Counter(nltk.word_tokenize(comment))
-#
-#def bigrams_phi_c(comment):
-#    words = nltk.word_tokenize(comment)
-#    return Counter(zip(["<s>"] + words, words + ["</s>"]))
-#
-#
-#
-#def embed_sum_phi_c(comment, embeddings):
-#    words = nltk.word_tokenize(comment)
-#    unk = np.zeros(next(iter(embeddings.values())).shape)
-#    return np.sum([embeddings[w] if w in embeddings else unk for w in words], axis=0)
-#
-#
-
-# Simply concatenate vectors from responses - hardcoded for balanced case
-#def concat_phi_r(response_features_pair):
-#    assert len(response_features_pair) == 2
-#    # print(response_features_pair[0].shape, response_features_pair[1].shape)
-#    cat = np.concatenate((response_features_pair[0], response_features_pair[1]))
-#    return cat
-
-
-
 # By the time examples are passed to classifiers for training or classification,
 # they should already have contextual information included in their representations
 # such that each x and y are effectively independent of each other
@@ -125,19 +99,3 @@ class MaxEntClassifier(SarcasmClassifier):
 
 
 
-
-
-
-
-
-#def fit_maxent_classifier(X, y, C=1.0):
-#    #print(X.shape, y.shape)
-#    mod = LogisticRegression(fit_intercept=True, C=C)
-#    mod.fit(X, y)
-#    return mod
-#
-#def fit_naive_bayes_classifier(X, y):
-#    #print(X.shape, y.shape)
-#    mod = MultinomialNB()
-#    mod.fit(X, y)
-#    return mod
