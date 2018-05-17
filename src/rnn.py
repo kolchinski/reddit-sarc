Attempted fix for in-place modification issues for CUDA gradients

src/rnn.py
@@ -104,6 +104,7 @@ class SarcasmRNN(nn.Module):
             raise ValueError("Need subreddit features for forward")
 
         batch_size = inputs.shape[0]
+        max_len = inputs.shape[1]
 
         embedded_inputs = self.embeddings(inputs)
         embedded_inputs_reversed = self.embeddings(inputs_reversed)
@@ -125,20 +126,25 @@ class SarcasmRNN(nn.Module):
             final_states_f = torch.gather(rnn_states_f, 1, idx).squeeze()
             final_states_b = torch.gather(rnn_states_b, 1, idx).squeeze()
             final_states = torch.cat((final_states_f, final_states_b), 1)
-
         else:
             #Apply attention!
-            reversed_states_b = torch.zeros_like(rnn_states_b)
-            for i in range(reversed_states_b.shape[0]):
+            #reversed_states_b = torch.zeros_like(rnn_states_b)
+            reversed_states_b = None
+            for i in range(batch_size):
                 # Flip every reverse-RNN set of outputs in the batch...
-                l = lengths[i]
+                l = int(lengths[i])
                 # Zero out places where the RNN ran over the end of the sequence:
                 rnn_states_f[i, :l] = 0
-                rnn_states_b[i, :l] = 0
 
                 reversed_indices = torch.LongTensor([j for j in range(l - 1, -1, -1)]).to(self.device)
                 inverted_tensor = torch.index_select(rnn_states_b[i], 0, reversed_indices)
-                reversed_states_b[i, :l] = inverted_tensor
+                padding = torch.zeros((max_len - l, self.hidden_dim), dtype=torch.float).to(self.device)
+                #print(inverted_tensor.shape, padding.shape)
+                inverted_tensor = torch.cat((inverted_tensor, padding),0).unsqueeze(0)
+                #print(inverted_tensor.shape)
+                if reversed_states_b is None: reversed_states_b = inverted_tensor
+                else: reversed_states_b = torch.cat((reversed_states_b, inverted_tensor),0)
+                #reversed_states_b[i, :l] = inverted_tensor
 
             rnn_states = torch.cat((rnn_states_f, reversed_states_b), 2)
             u = torch.tanh(torch.matmul(rnn_states, self.W_omega) + self.b_omega)