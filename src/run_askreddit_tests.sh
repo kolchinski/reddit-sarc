724ab90642ef95bf7d4a1c90acdd00dd62ff7028
Alex Kolchinski <y.a.kolchinski@gmail.com>
2018-05-22 07:18:42 +0000

    Tests on AskReddit data

---
+++ src/run_askreddit_tests.sh
@@ -0,0 +1,7 @@
+
+#python main.py F2 >> F2_TEST_LOG.txt
+#python main.py F3 >> F3_TEST_LOG.txt
+#python main.py F4 >> F4_TEST_LOG.txt
+python main.py G2 >> G2_TEST_LOG.txt
+python main.py G3 >> G3_TEST_LOG.txt
+python main.py G4 >> G4_TEST_LOG.txt
