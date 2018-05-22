a4529f7fb7f16eb13c319e23d557132a39aab7d2
Alex Kolchinski <y.a.kolchinski@gmail.com>
2018-05-21 17:34:41 -0700

    Added configs for AskReddit tests

--- src/test_configs.py
+++ src/test_configs.py
@@ -85,6 +85,7 @@ B4 = {
     "output_graphs" : False
 }
 
+
 C2 = {
     "dataset_splitter" : split_dataset_random_plus_politics,
     "test_splitter" : split_dataset_val_only_01,
@@ -259,6 +260,46 @@ D4 = {
 }
 
 
+def askreddit_reader():
+    return sarc_reader(FULL_COMMENTS, FULL_TRAIN_BALANCED, False, 'AskReddit')
+def askreddit_test_reader():
+    return sarc_reader(FULL_COMMENTS, FULL_TEST_BALANCED, False, 'AskReddit')
+
+def askreddit_reader_unbalanced():
+    return sarc_reader(FULL_COMMENTS, FULL_TRAIN_UNBALANCED, False, 'AskReddit')
+def askreddit_test_reader_unbalanced():
+    return sarc_reader(FULL_COMMENTS, FULL_TEST_UNBALANCED, False, 'AskReddit')
+
+F2 = B2.copy()
+F2['data_reader'] = askreddit_reader
+F2['test_reader'] = askreddit_test_reader
+
+F3 = B3.copy()
+F3['data_reader'] = askreddit_reader
+F3['test_reader'] = askreddit_test_reader
+
+F4 = B4.copy()
+F4['data_reader'] = askreddit_reader
+F4['test_reader'] = askreddit_test_reader
+
+
+G2 = D2.copy()
+G2['data_reader'] = askreddit_reader_unbalanced
+G2['test_reader'] = askreddit_test_reader_unbalanced
+
+G3 = D3.copy()
+G3['data_reader'] = askreddit_reader_unbalanced
+G3['test_reader'] = askreddit_test_reader_unbalanced
+
+G4 = D4.copy()
+G4['data_reader'] = askreddit_reader_unbalanced
+G4['test_reader'] = askreddit_test_reader_unbalanced
+
+
+
+
+
+
 test_configs = {
     'B2' : B2,
     'B3' : B3,
@@ -268,7 +309,14 @@ test_configs = {
     'C4' : C4,
     'D2' : D2,
     'D3' : D3,
-    'D4' : D4
+    'D4' : D4,
+
+    'F2' : F2,
+    'F3' : F3,
+    'F4' : F4,
+    'G2' : G2,
+    'G3' : G3,
+    'G4' : G4,
 }
 
 
