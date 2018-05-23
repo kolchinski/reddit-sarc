3e754fe019b3f363c1c7bd9f0fb0e463383a7abf
Alex Kolchinski <y.a.kolchinski@gmail.com>
2018-05-22 20:18:12 -0700

    Added significance tests script

--- logs/significance_tests.py
+++ logs/significance_tests.py
@@ -2,6 +2,15 @@ import numpy as np
 from scipy.stats import wilcoxon
 from itertools import combinations
 
+
+def bootstrap_samples(l, n):
+    return np.mean(np.random.choice(l, (n,len(l))), axis = 1)
+
+def bootstrap_ci(l):
+    samples = sorted(bootstrap_samples(l, 1000))
+    return np.round(samples[50],3), np.round(samples[950],3)
+
+
 dataset_letters = ('B', 'C', 'D', 'F', 'G')
 model_numbers = ('2', '3', '4')
 
@@ -18,10 +27,14 @@ for a in dataset_letters:
                     cur_f1s = [float(x) for x in line[line.index('[') + 1 : -2].split(',')]
                     f1s += cur_f1s
         print(len(f1s), np.mean(f1s))
+        print(bootstrap_ci(f1s), '\n')
         f1s_dict[a+b] = f1s
 
 #print(f1s_dict)
+print(f1s_dict['G3'])
 
+
+'''
 for dataset_letter in dataset_letters:
     print('Evaluating for column {}'.format(dataset_letter))
     for model_pair in combinations(model_numbers, 2):
@@ -30,3 +43,4 @@ for dataset_letter in dataset_letters:
         print(wilcoxon(f1s_dict[idx1], f1s_dict[idx2]))
         print('\n')
     print('\n')
+'''
